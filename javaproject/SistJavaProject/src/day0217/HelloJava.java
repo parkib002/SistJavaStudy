a04a0716784b86ccd70d74d92893e40dd1b0c57d
parkib002 <parkib003@naver.com>
2025-05-07 20:15:55 +0900

    delete test

--- javaproject/SistJavaProject/src/day0217/HelloJava.java
+++ javaproject/SistJavaProject/src/day0217/HelloJava.java
@@ -6,6 +6,6 @@ public class HelloJava {
 		
 		System.out.println("오늘은 자바를 시작하는 날");
 		System.out.println("잘들 배워봅시다!!!Strat");
-		System.out.println("git test");
+		//System.out.println("git test");
 	}
 }
