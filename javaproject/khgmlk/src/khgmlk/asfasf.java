0f3f24142f7c440df7c313069bca7b8f329d66d3
parkib002 <parkib003@naver.com>
2025-05-07 12:08:04 +0900

    test

---
+++ javaproject/khgmlk/src/khgmlk/asfasf.java
@@ -0,0 +1,10 @@
+package khgmlk;
+
+public class asfasf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("asdasd");
+	}
+
+}
---
+++ javaproject/test/src/bbbb/nnnn.java
@@ -0,0 +1,10 @@
+package bbbb;
+
+public class nnnn {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("nnnn");
+	}
+
+}
