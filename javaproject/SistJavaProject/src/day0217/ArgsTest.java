01770a2dec81a4c5862b069cee170e7a3d11ec22
parkib002 <parkib003@naver.com>
2025-05-08 13:47:15 +0900

    test

--- javaproject/SistJavaProject/src/day0217/ArgsTest.java
+++ javaproject/SistJavaProject/src/day0217/ArgsTest.java
@@ -6,8 +6,8 @@ public class ArgsTest {
 		// TODO Auto-generated method stub
 
 //		System.out.println("첫번째: "+args[0]);
-		System.out.println("두번째: "+args[1]);
-		System.out.println(args[0]+" "+args[1]);
+//		System.out.println("두번째: "+args[1]);
+//		System.out.println(args[0]+" "+args[1]);
 	}
 
 }
